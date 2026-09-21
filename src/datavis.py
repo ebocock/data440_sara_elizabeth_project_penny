6a78f682e9dac0fedecd4efc62c54d2b23f4e233
ebocock <bocockelizabeth@gmail.com>
2026-09-21 12:48:55 -0400

    generating dummy data in datavis.py

--- src/datavis.py
+++ src/datavis.py
@@ -3,6 +3,12 @@ import seaborn as sns
 import numpy as np
 import matplotlib
 
+#generate some dummy data
+regular_wins = np.random.rand(8, 8)
+ron_wins = np.random.rand(8,8)
+regular_ties = np.random.rand(8,8)
+ron_ties = np.random.rand(8,8)
+
 #function to get the data
 
 #function to do division
