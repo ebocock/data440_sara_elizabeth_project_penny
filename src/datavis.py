3321e58650bb622550557bdebc2b11898a2058ea
ebocock <bocockelizabeth@gmail.com>
2026-09-21 12:43:46 -0400

    updated the uv and created the .py file for visualization

---
+++ src/datavis.py
@@ -0,0 +1,14 @@
+# makes visualizations from output matrices
+import seaborn as sns
+import numpy as np
+import matplotlib
+
+#function to get the data
+
+#function to do division
+
+#function to make visualization
+
+#function to update visualization
+
+#function to show visualization
